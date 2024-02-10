00d5205a7825ae2a7163f1738d6f02e4b1ff17ce
Goupil <arthur.goupil@blablacar.com>
2024-02-10 19:36:36 +0100

    remove try/catch blocks

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -3,29 +3,23 @@ import * as Items from "../../../models/Items";
 import { Op } from "sequelize";
 
 export async function GET(request: Request) {
-  try {
-    await sequelize.authenticate();
+  await sequelize.authenticate();
 
-    const { searchParams } = new URL(request.url);
-    const name = searchParams.get("name");
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get("name");
 
-    if (!name) {
-      throw new Error("name not provided");
-    }
-
-    const items = await Items.Items.findAll({
-      where: {
-        name: {
-          [Op.like]: `%${name}%`,
-        },
-        deleted_at: null,
-      },
-    });
-
-    return Response.json(
-      items.map((item) => ({ name: item.name, id: item.id }))
-    );
-  } catch (error) {
-    console.error(error);
+  if (!name) {
+    throw new Error("name not provided");
   }
+
+  const items = await Items.Items.findAll({
+    where: {
+      name: {
+        [Op.like]: `%${name}%`,
+      },
+      deleted_at: null,
+    },
+  });
+
+  return Response.json(items.map((item) => ({ name: item.name, id: item.id })));
 }
--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,15 +1,11 @@
 import { NextApiRequest } from "next";
 
 export async function POST(request: Request) {
-  try {
-    const body = await request.json();
+  const body = await request.json();
 
-    if (!body.password) {
-      throw new Error("password not provided");
-    }
-
-    return Response.json({ isAllowed: body.password === process.env.PASSWORD });
-  } catch (error) {
-    console.error(error);
+  if (!body.password) {
+    throw new Error("password not provided");
   }
+
+  return Response.json({ isAllowed: body.password === process.env.PASSWORD });
 }
